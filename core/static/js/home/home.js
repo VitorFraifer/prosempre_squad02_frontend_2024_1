a91cb4f1f6e28799422361275861d834ab4a24c5
Vitor Fraifer <vitorfraifer10@gmail.com>
2024-04-03 20:03:03 -0300

    Feat: implementei carrossel na home

--- core/static/js/home/home.js
+++ core/static/js/home/home.js
@@ -1,26 +1,38 @@
+function updateBolaV(index) {
+  const bolas = document.querySelectorAll('.bolaV');
+  bolas.forEach((bola, i) => {
+      if (i === index) {
+          bola.style.backgroundColor = '#262626';
+      } else {
+          bola.style.backgroundColor = '#E1E5EE';
+      }
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    const slides = document.querySelectorAll('.nossaHistoriaImg');
-    let currentIndex = 0;
-  
-    function showSlide(index) {
-      slides.forEach((slide, i) => {
-        if (i === index) {
-          slide.style.transform = 'translateX(0)';
-        } else if (i < index) {
-          slide.style.transform = 'translateX(-100%)';
-        } else {
-          slide.style.transform = 'translateX(100%)';
-        }
-      });
-    }
-  
-    function nextSlide() {
+  const slides = document.querySelectorAll('.nossaHistoriaImg');
+  let currentIndex = 0;
+  let intervalo = 3000;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.style.transform = 'translateX(0)';
+      } else if (i < index) {
+        slide.style.transform = 'translateX(-100%)';
+      } else {
+        slide.style.transform = 'translateX(100%)';
+      }
+    });
+    updateBolaV(index);
+  }
+
+  function nextSlide() {
       currentIndex = (currentIndex + 1) % slides.length;
       showSlide(currentIndex);
-    }
-  
-    setInterval(nextSlide, 2000); // Altere o intervalo conforme necessário (em milissegundos)
-  
-    showSlide(currentIndex);
-  });
-  
+  }
+
+  setInterval(nextSlide, intervalo); // Altere o intervalo conforme necessário (em milissegundos)
+
+  showSlide(currentIndex);    
+});
